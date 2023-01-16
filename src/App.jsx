906e05b4d0e7efa966926019af8544ad7a7d16dd
mateuscatao <mateuscatao1311@gmail.com>
2023-01-16 20:19:00 -0300

    Componentes Sidebar e Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import { Header } from './components/Header';
 import { Post } from './Post';
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css'
+
+import './global.css';
 
 export function App() {
   return (
-    <Post />
+    <div>
+      <Header />
+  
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Mateus Catão"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam unde accusantium minima delectus nostrum at officia itaque distinctio id obcaecati fugit officiis quae a aliquid, porro modi ducimus natus nihil."
+          />
+          <Post
+            author="John Doe"
+            content="Um novo post."
+          />
+        </main>
+      </div>
+    </div>
   )
 }
