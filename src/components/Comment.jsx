7865492480f41c5fc1f03739986cbde9ac153481
mateuscatao <mateuscatao1311@gmail.com>
2023-01-31 17:48:05 -0300

    useState e programação declarativa nos comentários

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,7 @@ import { ThumbsUp, Trash } from 'phosphor-react'
 import { Avatar } from './Avatar'
 import styles from './Comment.module.css'
 
-export function Comment() {2222
+export function Comment({ content }) {
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/51727745?v=4" />
@@ -20,7 +20,7 @@ export function Comment() {2222
             </button>
           </header>
 
-          <p>Muito bom Devon, parabéns!! 👏👏</p>
+          <p>{content}</p>
         </div>
 
         <footer>
